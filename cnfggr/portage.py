7f6063576ce412466368cb53587e3e13b9f6bac0
Jon Eyolfson <jon@eyl.io>
2014-03-09 17:29:37 -0400

    Added portage copyright notice

--- cnfggr/portage.py
+++ cnfggr/portage.py
@@ -1,3 +1,20 @@
+# Copyright 2014 Jon Eyolfson
+# Copyright 2014 Jean-Christophe Petkovich
+#
+# This file is part of Cnfggr.
+#
+# Cnfggr is free software: you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# Cnfggr is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# Cnfggr. If not, see <http://www.gnu.org/licenses/>.
+
 import subprocess
 import os
 
