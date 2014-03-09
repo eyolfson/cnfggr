c8fcd6729ef321ed9e3af5e18d2c9b199294248e
Jon Eyolfson <jon@eyl.io>
2014-03-09 15:49:31 -0400

    Version switched back to development

--- cnfggr/version.py
+++ cnfggr/version.py
@@ -29,7 +29,7 @@ class Version(namedtuple('Version', ['major', 'minor', 'patch', 'extra'])):
         return s
 
 def get_version():
-    version = Version(0, 0, 3, '')
+    version = Version(0, 0, 3, 'development')
 
     if version.extra != 'development':
         return version
